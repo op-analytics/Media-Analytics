35b9b3d6a274332d2e3f4d35003716e7e23b1b63
nichtj3 <nichtj3@student.op.ac.nz>
2019-08-05 17:15:12 +1200

    Remove unneeded comment

--- back-end/app/nlp/controllers.py
+++ back-end/app/nlp/controllers.py
@@ -4,7 +4,6 @@ from flask import Blueprint, jsonify
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 nlp = Blueprint('nlp', __name__, url_prefix='/nlp')
 
-# Set the route and accepted methods
 @nlp.route('/', methods=['GET'])
 def index():
     return jsonify(
