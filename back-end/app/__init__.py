46d4abbe27605c9e856d623442a76a1c4faaf40f
nichtj3 <nichtj3@student.op.ac.nz>
2019-08-05 17:17:13 +1200

    Add api blueprint

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -17,7 +17,9 @@ def not_found(error):
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.nlp.controllers import nlp as nlp_module
+from app.api.controllers import api as api_module
 
 # Register blueprint(s)
+app.register_blueprint(api_module)
 app.register_blueprint(nlp_module)
 # app.register_blueprint(xyz_module)
---
+++ back-end/app/api/controllers.py
@@ -0,0 +1,11 @@
+# Import flask dependencies
+from flask import Blueprint, jsonify
+
+api = Blueprint('api',__name__,)
+
+@api.route('/', methods=['GET'])
+def index():
+    return jsonify(
+                code=202,
+                message="Hello from Media Analytics API"
+            )
