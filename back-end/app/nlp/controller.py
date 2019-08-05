96ccca6f74f8f8572e85df259ad9bb9b7e6ff503
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2019-08-05 21:33:38 +1200

    Change jsonify receive a more json like object in nlp controller

--- back-end/app/nlp/controller.py
+++ back-end/app/nlp/controller.py
@@ -6,7 +6,7 @@ nlp = Blueprint('nlp', __name__)
 
 @nlp.route('/', methods=['GET'])
 def index():
-    return jsonify(
-                code=202,
-                message="Hello from nlp"
-            )
+    return jsonify({
+        code:202,
+        message:"Hello from nlp"
+    })
