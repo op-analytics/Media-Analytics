7f5d7eb6c563e8d4318020569978687b4ee120b1
nichtj3 <nichtj3@student.op.ac.nz>
2019-08-06 12:48:44 +1200

    Add comments to nlp routes test

--- back-end/app/nlp/test_routes.py
+++ back-end/app/nlp/test_routes.py
@@ -3,16 +3,19 @@ from app import app
 
 
 class TestNLPRoutes(object):
+    # Put any setup code in this method
     @classmethod
     def setup_class(self):
         self.app = app
         self.prefix = '/nlp/'
         self.client = self.app.test_client()
 
+    # Put any cleanup code in this method
     @classmethod
     def teardown_class(self):
         pass
 
+    # Tests if the message returned from the nlp index contains the string nlp
     def test_index_route_returns_nlp(self):
         response = self.client.get(self.prefix).get_json()
         assert 'nlp' in response['message']
