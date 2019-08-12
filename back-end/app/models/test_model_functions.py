18879efcf5be345a758ded4a2c43862b72ff2912
nichtj3 <nichtj3@student.op.ac.nz>
2019-08-12 13:52:56 +1200

    Update test name

--- back-end/app/models/test_model_functions.py
+++ back-end/app/models/test_model_functions.py
@@ -3,7 +3,7 @@ from app.models.model_functions import loadModels
 
 
 class TestModelFunctions(object):
-    def test_model_functions_returns_a_dictionary_when_given_a_valid_path(self):
+    def test_returns_a_dictionary(self):
         '''Test if loadModels Returns a dictionary'''
         models = loadModels('./')
         assert isinstance(models,dict)
