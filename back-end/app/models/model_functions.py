0a10b3f0688b13af5bc818d87ca7c7b5c694180b
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-08-13 13:45:25 +1200

    Check if path exists when trying to load models

--- back-end/app/models/model_functions.py
+++ back-end/app/models/model_functions.py
@@ -1,5 +1,11 @@
+import os
+
+
 def loadModels(path):
     '''Loads and returns all models in the given path as a dictionary'''
+    # Check if path isn't valid raises os error if so
+    if not os.path.exists(path):
+        raise OSError("given path %s does not exist" % path)
     return {
-            "ohno":1
-            }
+        "ohno": 1
+    }
--- back-end/app/models/test_model_functions.py
+++ back-end/app/models/test_model_functions.py
@@ -1,8 +1,22 @@
 import pytest
+import string
+import os
+from random import *
 from app.models.model_functions import loadModels
 
 
 class TestModelFunctions(object):
     def test_returns_a_dictionary(self):
         models = loadModels('./')
-        assert isinstance(models,dict)
+        assert isinstance(models, dict)
+
+    def test_should_throw_an_error_given_a_path_that_doesnt_exist(self):
+        # Create random path
+        path = ''
+        allchar = string.ascii_letters + string.punctuation + string.digits
+        while os.path.exists(path):
+            path = '/'+"".join(choice(allchar)
+                               for x in range(randint(3, 8)))
+        # Test OSError is raised when given an invalid path
+        with pytest.raises(OSError):
+            models = loadModels(path)
