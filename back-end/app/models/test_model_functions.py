b2a470af02bff26120e3a6c8951dcc337b84fc8e
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-08-13 13:27:20 +1200

    Remove unneeded comment from test file

--- back-end/app/models/test_model_functions.py
+++ back-end/app/models/test_model_functions.py
@@ -4,6 +4,5 @@ from app.models.model_functions import loadModels
 
 class TestModelFunctions(object):
     def test_returns_a_dictionary(self):
-        '''Test if loadModels Returns a dictionary'''
         models = loadModels('./')
         assert isinstance(models,dict)
