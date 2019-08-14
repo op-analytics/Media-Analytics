5d0ef89032f7f6f8f49cc7bfe765edf9d37ef89e
Zimbardoex <Zimbardoex@gmail.com>
2019-08-15 06:44:39 +1200

    Add LineGraph to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import LineGraph from './Timeline/components/LineGraph';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Media analytics site</h1>
+      <LineGraph />
     </div>
   );
 }
