615f0cf8270b60ede9a516d95922dbf6747ad96a
Zimbardoex <Zimbardoex@gmail.com>
2019-08-16 02:09:16 +1200

    Update App.js and remove LineGraph component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import LineGraph from './Timeline/components/LineGraph';
+import Timeline from './Timeline/Timeline';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Media analytics site</h1>
-      <LineGraph />
+      <Timeline />
     </div>
   );
 }
