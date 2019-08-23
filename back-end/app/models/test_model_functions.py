f46c37f7d050ca36c9b9b949d68359f0f64e05c8
Trent Nicholson <nichtj3@student.op.ac.nz>
2019-08-23 23:50:08 +1200

    update model tests to use mocking in most places

--- back-end/app/models/test_model_functions.py
+++ back-end/app/models/test_model_functions.py
@@ -3,39 +3,35 @@ import string
 from gensim.models import Word2Vec
 import secrets
 import os
-from random import *
+from random import randint, choice
 from app.models.model_functions import loadModels
+import unittest.mock as mock
 
 
 class TestModelFunctions(object):
-    def test_should_throw_an_error_given_a_path_that_doesnt_exist(self):
-        # Create random path
-        path = ''
-        allchar = string.ascii_letters + string.punctuation + string.digits
-        while os.path.exists(path):
-            path = '/'+"".join(choice(allchar)
-                               for x in range(randint(3, 8)))
+    @mock.patch('app.models.model_functions.os.path')
+    def test_should_throw_an_error_given_a_path_that_doesnt_exist(self,mock_path):
         # Test OSError is raised when given an invalid path
+        mock_path.exists.return_value = False
         with pytest.raises(OSError):
-            models = loadModels(path)
+          loadModels('')
 
-    def test_should_throw_an_error_given_a_path_is_a_file(self):
-        # Get random file from files in current dir
-        file = secrets.choice(
-            [f for f in os.listdir('.') if os.path.isfile(f)])
+    @mock.patch('app.models.model_functions.os.path')
+    def test_should_throw_an_error_given_a_path_is_a_file(self,mock_path):
         # Test OSError is raised when given a file for a path
+        mock_path.isfile.return_value = True
         with pytest.raises(OSError):
-            models = loadModels(file)
+            loadModels('')
 
-    def test_should_throw_an_error_when_given_path_has_no_models(self):
+    @mock.patch('app.models.model_functions.os.listdir')
+    def test_should_throw_an_error_when_given_path_has_no_models(self,mock_listdir):
+        mock_listdir.return_value = [
+            'notamodel.py', 'notamodel-example', 'notamodel.xml', '.gitignore']
         with pytest.raises(OSError):
-            models = loadModels('.')
+          loadModels('')
 
+    # TODO: Find a way to mock models used here
+    # MAYBE: create an array of files using mock open called years holding a numpy array Use MOCK_OPEN and copy small models contents to be used as files read data
     def test_should_return_a_dictionary_of_models_given_a_valid_path_from_env(self):
         models = loadModels(os.environ['TIMELINE_MODELS_DIR'])
-        are_all_models = True
-        for model in models.values():
-            if not isinstance(model, Word2Vec):
-                are_all_models = False
-        assert are_all_models
-
+        assert all(isinstance(model, Word2Vec) for model in models.values())
