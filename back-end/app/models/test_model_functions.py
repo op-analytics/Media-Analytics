e4df51a1ac7796af70217245e1cb9ef64a0bbbf2
Trent Nicholson <nichtj3@student.op.ac.nz>
2019-08-24 01:51:59 +1200

    Update model tests to use mocking so they run faster

--- back-end/app/models/test_model_functions.py
+++ back-end/app/models/test_model_functions.py
@@ -1,37 +1,40 @@
 import pytest
-import string
 from gensim.models import Word2Vec
-import secrets
-import os
-from random import randint, choice
 from app.models.model_functions import loadModels
 import unittest.mock as mock
 
 
 class TestModelFunctions(object):
     @mock.patch('app.models.model_functions.os.path')
-    def test_should_throw_an_error_given_a_path_that_doesnt_exist(self,mock_path):
-        # Test OSError is raised when given an invalid path
+    def test_should_throw_an_error_given_a_path_that_doesnt_exist(self, mock_path):
         mock_path.exists.return_value = False
         with pytest.raises(OSError):
-          loadModels('')
+            loadModels('')
 
     @mock.patch('app.models.model_functions.os.path')
-    def test_should_throw_an_error_given_a_path_is_a_file(self,mock_path):
-        # Test OSError is raised when given a file for a path
+    def test_should_throw_an_error_given_a_path_is_a_file(self, mock_path):
+        mock_path.exists.return_value = True
         mock_path.isfile.return_value = True
         with pytest.raises(OSError):
             loadModels('')
 
+    @mock.patch('app.models.model_functions.os.path')
     @mock.patch('app.models.model_functions.os.listdir')
-    def test_should_throw_an_error_when_given_path_has_no_models(self,mock_listdir):
+    def test_should_throw_an_error_when_given_path_has_no_models(self, mock_listdir, mock_path):
         mock_listdir.return_value = [
             'notamodel.py', 'notamodel-example', 'notamodel.xml', '.gitignore']
+        mock_path.exists.return_value = True
+        mock_path.isfile.return_value = False
         with pytest.raises(OSError):
-          loadModels('')
+            loadModels('')
 
-    # TODO: Find a way to mock models used here
-    # MAYBE: create an array of files using mock open called years holding a numpy array Use MOCK_OPEN and copy small models contents to be used as files read data
-    def test_should_return_a_dictionary_of_models_given_a_valid_path_from_env(self):
-        models = loadModels(os.environ['TIMELINE_MODELS_DIR'])
+    @mock.patch('app.models.model_functions.os.path.exists', return_value=True)
+    @mock.patch('app.models.model_functions.os.path.isfile', return_value=False)
+    @mock.patch('app.models.model_functions.os.path.isdir', return_value=False)
+    @mock.patch('app.models.model_functions.os.listdir', return_value=['1991'])
+    @mock.patch('app.models.model_functions.KeyedVectors.load')
+    def test_should_return_a_dictionary_of_models(self, mock_load_keyedvectors, *args, **kwargs):
+        mock_load_keyedvectors.return_value = Word2Vec([['first', 'sentence'], [
+                                                       'second', 'sentence']], min_count=1, batch_words=0.1, workers=1)
+        models = loadModels('')
         assert all(isinstance(model, Word2Vec) for model in models.values())
