2e34b7c4ee3bb323f7968653f62f9da4371a15e6
Zimbardoex <Zimbardoex@gmail.com>
2019-08-26 16:11:54 +1200

    Add two test for timeline-form component

---
+++ front-end/src/Timeline/components/Timeline-form.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Form from './Timeline-form';
+
+describe('Form', () => {
+  it('should render a form', () => {
+    const component = shallow(<Form />);
+    expect(component.find('form').exists());
+  });
+
+  it('form fields should be fillable', () => {
+    const component = shallow(<Form />);
+    const testData = { yearFrom: '1980', yearTo: '1990', word: 'man' };
+
+    const wordInput = component.find('Word');
+    wordInput.value = testData.word;
+    expect(wordInput.value).toBe('man');
+
+    const yearFromInput = component.find('year-from');
+    yearFromInput.value = testData.yearFrom;
+    expect(yearFromInput.value).toBe('1980');
+
+    const yearToInput = component.find('year-to');
+    yearToInput.value = testData.yearTo;
+    expect(yearToInput.value).toBe('1990');
+  });
+});
