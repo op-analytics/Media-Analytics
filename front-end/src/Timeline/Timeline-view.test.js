cd0b9099a58ab2a15423712096c05f907930a6e3
Zimbardoex <Zimbardoex@gmail.com>
2019-08-26 16:11:17 +1200

    Change test name to 'Form' from 'form'. Add test for Grid

--- front-end/src/Timeline/Timeline-view.test.js
+++ front-end/src/Timeline/Timeline-view.test.js
@@ -3,8 +3,15 @@ import { shallow } from 'enzyme';
 import Timeline from './Timeline-view';
 
 describe('Timeline', () => {
-  it('should render a form', () => {
+  it('should render a Form', () => {
     const component = shallow(<Timeline />);
     expect(component.find('Form').exists());
+  });
+});
+
+describe('Timeline', () => {
+  it('should render a Frid', () => {
+    const component = shallow(<Timeline />);
+    expect(component.find('Grid').exists());
   });
 });
