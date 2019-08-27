e7f0d4ea4202861e815d95a4164078a77c7d75c8
Zimbardoex <Zimbardoex@gmail.com>
2019-08-27 19:01:05 +1200

    Add test for timeline-form rendering correctly

--- front-end/src/Timeline/components/Timeline-form.test.js
+++ front-end/src/Timeline/components/Timeline-form.test.js
@@ -2,25 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Form from './Timeline-form';
 
+const props = {
+  setTimelineData: jest.fn(),
+};
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const getWrapper = () => shallow(<Form {...props} />);
+
 describe('Form', () => {
-  it('should render a form', () => {
-    const component = shallow(<Form />);
-    expect(component.find('form').exists());
+  it('should render a <form>', () => {
+    expect(getWrapper().exists('form')).toBe(true);
   });
 
-  it('is fillable when', () => {
-    const component = shallow(<Form />);
+  it('renders correctly', () => {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    const component = shallow(<Form {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test('is fillable', () => {
     const testData = { yearFrom: '1980', yearTo: '1990', word: 'man' };
 
-    const wordInput = component.find('Word');
+    const wordInput = getWrapper().find('Word');
     wordInput.value = testData.word;
     expect(wordInput.value).toBe('man');
 
-    const yearFromInput = component.find('year-from');
+    const yearFromInput = getWrapper().find('year-from');
     yearFromInput.value = testData.yearFrom;
     expect(yearFromInput.value).toBe('1980');
 
-    const yearToInput = component.find('year-to');
+    const yearToInput = getWrapper().find('year-to');
     yearToInput.value = testData.yearTo;
     expect(yearToInput.value).toBe('1990');
   });
