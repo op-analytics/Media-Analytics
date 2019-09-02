517ad68800c124871f0a388e7025dacb99a7d8ed
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2019-09-02 15:17:25 +1200

    Add nav bar (#57)
    * Add material-ui drawer nav bar with tests
    
    * Add material ui icons as dependency

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Timeline from './Timeline/Timeline-view';
+import Nav from './Shared/Nav';
 
 function Home() {
   return (
@@ -19,14 +20,31 @@ function NotFound() {
   );
 }
 
+const links = [
+  [
+    { href: '/', text: 'Home' },
+    { href: '/timeline', text: 'Timeline' },
+    { href: '/sentiment-analasis', text: 'Sentiment Analysis' },
+    { href: '/topic-modeling', text: 'Topic Modeling' },
+    { href: '/latent-association', text: 'Latent Association' },
+  ],
+  [
+    { href: '/about', text: 'About' },
+    { href: '/docs', text: 'Docs' },
+    { href: '/paper', text: 'Paper' },
+  ],
+];
+
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/timeline" component={Timeline} />
-        <Route component={NotFound} />
-      </Switch>
+      <Nav title="NYT Analytics" links={links}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/timeline" component={Timeline} />
+          <Route component={NotFound} />
+        </Switch>
+      </Nav>
     </Router>
   );
 }
---
+++ front-end/src/Shared/Nav.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Nav from './Nav';
+
+const links = [
+  [
+    { href: '/', text: 'Home' },
+    { href: '/timeline', text: 'Timeline' },
+    { href: '/sentiment-analasis', text: 'Sentiment Analysis' },
+    { href: '/topic-modeling', text: 'Topic Modeling' },
+    { href: '/latent-association', text: 'Latent Association' },
+  ],
+  [
+    { href: '/about', text: 'About' },
+    { href: '/docs', text: 'Docs' },
+    { href: '/paper', text: 'Paper' },
+  ],
+];
+
+const linksNoDivider = [
+  [
+    { href: '/', text: 'Home' },
+    { href: '/timeline', text: 'Timeline' },
+    { href: '/sentiment-analasis', text: 'Sentiment Analysis' },
+    { href: '/topic-modeling', text: 'Topic Modeling' },
+    { href: '/latent-association', text: 'Latent Association' },
+  ],
+];
+
+const getWrapperWithDivider = () => shallow(<Nav title="Title" links={links} />);
+
+const getWrapper = () => shallow(<Nav title="Title" links={linksNoDivider} />);
+
+describe('Nav', () => {
+  it('Should render', () => {
+    const nav = getWrapper();
+    expect(nav.exists('nav')).toEqual(true);
+  });
+
+  it('Should render correctly without divider', () => {
+    const nav = getWrapper();
+    expect(nav).toMatchSnapshot();
+  });
+
+  it('Should render correctly with divider', () => {
+    const nav = getWrapperWithDivider();
+    expect(nav).toMatchSnapshot();
+  });
+});
