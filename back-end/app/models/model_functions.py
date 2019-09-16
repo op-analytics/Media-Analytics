1309f58e776fd7d5884c0d14cde90ab3c9a0c07c
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-09-16 14:56:50 +1200

    Added threading to loading models

--- back-end/app/models/model_functions.py
+++ back-end/app/models/model_functions.py
@@ -1,8 +1,11 @@
 import os
-from gensim.models import KeyedVectors
 from functools import reduce
+from threading import Thread
+
+from gensim.models import KeyedVectors
 
 
+# pylint: disable=invalid-name
 def isPotentialModel(file):
     """
         Checks if a given file is a potential model
@@ -11,6 +14,22 @@ def isPotentialModel(file):
     return not (file.lower().endswith("-example") or os.path.isdir(file) or "." in file)
 
 
+def load_model(path, file):
+    """Tries to open the model at the given path and adds total word counts property"""
+    model = KeyedVectors.load(path + file, mmap="r")
+    model.syn0norm = model.wv.syn0  # prevent recalc of normed vectors
+    model.totalWordCount = reduce(
+        lambda total, word: total + word.count, model.wv.vocab.values(), 0
+    )
+    return model
+
+
+def add_model_to_models(path, file, models):
+    """Loads and adds a given model to the models dictionary"""
+    models[str(file)] = load_model(path, file)
+
+
+# pylint: disable=invalid-name
 def loadModels(path):
     """Loads and returns all models in the given path as a dictionary"""
     # raise OSError if path is invalid or is a file
@@ -18,18 +37,17 @@ def loadModels(path):
         raise OSError("given path does not exist")
     if os.path.isfile(path):
         raise OSError("given path is not a folder")
+    threads = []
     models = {}
     files = os.listdir(path)
     for file in files:
         # Only try open files that appear to be models
         if isPotentialModel(path + file):
-            # Try opening the model and add total word counts
-            model = KeyedVectors.load(path + file, mmap="r")
-            model.syn0norm = model.wv.syn0  # prevent recalc of normed vectors
-            model.totalWordCount = reduce(
-                lambda total, word: total + word.count, model.wv.vocab.values(), 0
-            )
-            models[str(file)] = model
-    if len(models) == 0:
+            process = Thread(target=add_model_to_models, args=[path, file, models])
+            process.start()
+            threads.append(process)
+    for process in threads:
+        process.join()
+    if not models:
         raise OSError("No models were found in given path")
     return models
