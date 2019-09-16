a598571db0c392f4609f26a70808d839f197b382
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-09-16 14:41:06 +1200

    Strip spaces from words
    * Create applymap util function that will apply an array of functions to
    another array
    * Use applymap function to lowercase and strip words in timeline routes

---
+++ back-end/app/utils.py
@@ -0,0 +1,5 @@
+def Applymap(functions, items):
+    """Applys an array of functions to an array of elements"""
+    for function in functions:
+        items = [function(item) for item in items]
+    return items
