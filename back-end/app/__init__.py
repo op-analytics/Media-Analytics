94e441a4103f6e023a9624dfe19e3a07cb06a90f
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2019-09-24 16:59:33 +1200

    Setup database (#74)
    * Add flask-mongoengine as dependency
    
    * Initialize DB in app factory

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,10 +1,11 @@
 # Import flask dependencies
 from flask import Flask, g, jsonify, request
 from flask_cors import CORS
+from flask_mongoengine import MongoEngine
 
 # Create Plugins
-# Example db = sqlAlchemy()
 cors = CORS()
+db = MongoEngine()
 
 
 def create_app(config_filename="config.Config"):
@@ -13,8 +14,8 @@ def create_app(config_filename="config.Config"):
     app.config.from_object(config_filename)
 
     # Initialize Plugins
-    # Example db.init_app(app)
     cors.init_app(app)
+    db.init_app(app)
 
     with app.app_context():
         # Import and Register blueprint(s)
