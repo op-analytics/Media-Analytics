2b663c7169c85229a82a05883f08c93cedb1147e
zimbardoex <zimbardoex@gmail.com>
2019-09-25 20:39:47 +1200

    Default to development config in run.py

--- back-end/run.py
+++ back-end/run.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 from app import create_app
 
-app = create_app()
+app = create_app('config.Dev')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=app.config["PORT"], debug=app.config["DEBUG"])
