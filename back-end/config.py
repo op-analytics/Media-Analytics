dfd0af34ddc1ea10e4b2ebc01c97c845bddff7a7
zimbardoex <zimbardoex@gmail.com>
2019-09-26 15:00:42 +1200

    Add testing mongo connection

--- back-end/config.py
+++ back-end/config.py
@@ -23,11 +23,12 @@ class Config:
 
 class Testing(Config):
     TESTING = True
+    MONGODB_SETTINGS = {
+        "db": "mongoenginetest",
+        "host": "mongomock://localhost",
+    }
 
 
 class Dev(Config):
     DEBUG = True
-    MONGODB_SETTINGS = {
-        "db": "nyta",
-        "host": "mongodb://localhost/nyta",
-    }
+    MONGODB_SETTINGS = {"db": "nyta", "host": "mongodb://localhost/nyta"}
