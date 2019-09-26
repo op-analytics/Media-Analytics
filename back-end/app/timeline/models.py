e5b9da1f36fbcff2fbda11ae5dd36105eaab2249
zimbardoex <zimbardoex@gmail.com>
2019-09-26 15:04:53 +1200

    Format with Black

--- back-end/app/timeline/models.py
+++ back-end/app/timeline/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class Sentiment(db.Document):
     year = db.IntField(required=True)
     word = db.StringField(required=True)
--- back-end/app/timeline/schemas.py
+++ back-end/app/timeline/schemas.py
@@ -1,12 +1,14 @@
 from marshmallow import Schema, ValidationError, fields, validates_schema, validate
 
 
-class FrequencySchema(Schema):
+class TimelineSchema(Schema):
     year_from = fields.Int(required=True)
     year_to = fields.Int(required=True)
-    words = fields.List(fields.String(), required=True, validates=validate.Length(min=1))
+    words = fields.List(
+        fields.String(), required=True, validates=validate.Length(min=1)
+    )
 
     @validates_schema(skip_on_field_errors=True)
-    def validate_year_from(self, data, *args, **kwargs):
+    def validate_year_from(self, data, *_, **__):
         if int(data["year_from"]) > int(data["year_to"]):
             raise ValidationError("year_from must be before year_to", "year_range")
