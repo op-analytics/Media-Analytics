8e85069a3e95033ed7ba745b80c6de8c6ad767ef
Zimbardoex <Zimbardoex@gmail.com>
2019-11-01 10:37:51 +1300

    Remove unneeded styles from About component

--- front-end/src/About/components/AboutSection.js
+++ front-end/src/About/components/AboutSection.js
@@ -1,39 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
 const useStyles = makeStyles({
-  about: {
-    marginBottom: '20px',
-  },
   aboutText: {
     paddingTop: 15,
-  },
-  contributorsContainer: {
-    display: 'flex',
-    paddingTop: 20,
-  },
-  card: {
-    width: '18vw',
-    marginRight: '10px',
-  },
-  image: {
-    width: '50%',
-    height: '50%',
-    maxWidth: 140,
-    margin: 10,
-    display: 'inline-block',
-  },
-  imageContainer: {
-    textAlign: 'center',
-  },
+  }
 });
 
 function AboutSection() {
   const classes = useStyles();
   return (
-    <Container className={classes.aboutContainer}>
+    <>
       <Typography variant="h4" component="h2">
         About
       </Typography>
@@ -49,7 +27,7 @@ function AboutSection() {
         clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
         amet.
       </Typography>
-    </Container>
+    </>
   );
 }
 
