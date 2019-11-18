eee885a66da3f131f24398ab7f3cf3ed136f87d6
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-11-18 15:30:00 +1300

    Add validation for .env files if there is an error you will be notified to check the console

---
+++ front-end/src/config/index.js
@@ -0,0 +1,29 @@
+import Joi from '@hapi/joi';
+import URLREGEX from './regexurl';
+
+const options = {
+  NODE_ENV: Joi.string()
+    .default('development')
+    .allow('development', 'test', 'production'),
+};
+
+if (process.env.NODE_ENV === 'development') {
+  options.REACT_APP_API_URL = Joi.string()
+    .regex(URLREGEX)
+    .required();
+}
+
+const schema = Joi.object(options).unknown(true);
+
+const { error, value: config } = schema.validate(process.env);
+
+if (error) {
+  alert(`Missing property in config, Check the console for more info`);
+  // eslint-disable-next-line no-console
+  console.error('Missing property in env.', error.message);
+}
+
+const API_URL =
+  config.NODE_ENV === 'production' ? '/api' : config.REACT_APP_API_URL;
+
+export { config, API_URL };
