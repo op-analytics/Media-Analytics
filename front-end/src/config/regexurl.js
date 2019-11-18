1e612f306b492d9eb47ebbab660e990e61ef7a2b
NICHTJ3 <nichtj3@student.op.ac.nz>
2019-11-18 15:31:25 +1300

    Add note for url regex

--- front-end/src/config/regexurl.js
+++ front-end/src/config/regexurl.js
@@ -1,3 +1,5 @@
+// NOTE: This does not check every edge case if you notice something please fix
+// it
 export default new RegExp(
   /((((https|ftp|http):(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/,
 );
