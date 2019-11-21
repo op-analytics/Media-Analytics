6bc26e80317fab361310764725f8201c16070db1
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2019-11-21 14:07:02 +1300

    Remove home component and redirect to frequency from / (#154)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Nav from './Shared/Nav';
 import FrequencyTimeline from './Timeline/Frequency-Timeline-view';
 import LatentAssociationTimeline from './Timeline/Latent-Association-view';
 import SentimentTimeline from './Timeline/Sentiment-Timeline-view';
-
-function Home() {
-  return (
-    <div className="App">
-      <h1>Media analytics site</h1>
-    </div>
-  );
-}
 
 function NotFound() {
   return (
@@ -24,7 +16,6 @@ function NotFound() {
 
 const links = [
   [
-    { href: '/', text: 'Home' },
     { href: '/frequency', text: 'Frequency Over Time' },
     { href: '/sentiment', text: 'Sentiment Over Time' },
     { href: '/topic-modeling', text: 'Topic Modeling' },
@@ -42,7 +33,7 @@ export default function App() {
     <Router>
       <Nav title="NYT Analytics" links={links}>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={() => <Redirect to="/frequency" />} />
           <Route path="/frequency" component={FrequencyTimeline} />
           <Route path="/sentiment" component={SentimentTimeline} />
           <Route
