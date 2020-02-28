672de9bc916514b4231f9894b34764387e6fe0e6
Zimbardoex <Zimbardoex@gmail.com>
2020-02-28 15:43:54 +1300

    Update get_word_frequency_data to use data in database instead of loaded models

--- back-end/app/timeline/utils.py
+++ back-end/app/timeline/utils.py
@@ -1,7 +1,7 @@
 from gensim import matutils
 from numpy import array, dot
 
-from .models import LatentAssociation
+from .models import LatentAssociation, Frequency
 
 
 def get_index_by_value(object_list, key, value):
@@ -49,28 +49,20 @@ def get_word_frequency_data(models, words, year_from, year_to):
                                   ]
     """
     frequency_data = []
-    for year, model in models.items():
-        year = int(year)
-        if year < year_from or year > year_to:
-            continue
-        for word in words:
-            word_exists = word in model.wv.vocab
-            word_count = model.wv.vocab[word].count if word_exists else 0
-            word_data = {
-                "year": year,
-                "rank": model.wv.vocab[word].index + 1 if word_exists else 0,
-                "count": word_count,
-                "freq": (word_count / model.totalWordCount) * 100 if word_exists else 0,
-            }
-            # Get the index of the object containing the current word in frequencyData.
-            data_index = get_index_by_value(frequency_data, "word", word)
-            # If the word is not already in frequencyData.
-            if data_index is None:
-                frequency_data.append({"word": word, "data": [word_data]})
-            else:
-                # If the word already exists in frequency data,
-                # just append the current year's data.
-                frequency_data[data_index]["data"].append(word_data)
+    frequency_data_db = Frequency.objects(
+        word__in=words, year__gte=year_from, year__lte=year_to
+    )
+
+    for frequency_data_obj in frequency_data_db:
+        word = frequency_data_obj.word
+        data_index = get_index_by_value(frequency_data, "word", word)
+        word_data = {"year": frequency_data_obj.year, "rank": frequency_data_obj.rank, "count": frequency_data_obj.count, "freq": frequency_data_obj.freq}
+        if data_index is None:
+            frequency_data.append({"word": word, "data": [word_data]})
+        else:
+            # If the word already exists in frequency data,
+            # just append the current year's data.
+            frequency_data[data_index]["data"].append(word_data)
     # Sort all words datasets by year
     for word_data in frequency_data:
         word_data["data"].sort(key=lambda x: int(x["year"]))
