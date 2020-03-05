9504a7fcbe754f2055d3020111af5f3e1ac0e67d
nichtj3 <nicholsontrent@gmail.com>
2020-03-06 12:38:36 +1300

    Create and test Login and signup views
    - Add storybook for testing
      - Setup storyshot
      - Add example stories
    - Add react-hook-forms

---
+++ front-end/src/Shared/LineChartsV2/linecharts.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import LineCharts from './index';
+
+export default {
+  title: 'LineCharts',
+  component: LineCharts,
+};
+
+const chartDatasetsWithOneWord = [
+  {
+    title: 'bob',
+    data: [{ y: 1, x: 2 }, { y: 0.3, x: 3 }, { y: 0.6, x: 7 }],
+    tooltipItems: [{ title: 'y', key: 'y' }],
+  },
+];
+
+const chartDatasetsWithTwoWords = [
+  {
+    title: 'bob',
+    data: [{ y: 1, x: 2 }, { y: 0.3, x: 3 }, { y: 0.6, x: 7 }],
+  },
+  {
+    title: 'bill',
+    data: [{ y: 1, x: 2 }, { y: 0.3, x: 3 }, { y: 0.6, x: 7 }],
+  },
+];
+
+const getProps = dataSource => ({
+  datasets: dataSource,
+  yAxisKey: 'y',
+  xAxisKey: 'x',
+  tooltipItems: [{ title: 'y', key: 'y' }],
+});
+
+export const LineChartWithOneDataSource = () => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <LineCharts {...getProps(chartDatasetsWithOneWord)} />
+);
+
+export const LineChartWithTwoDataSources = () => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <LineCharts {...getProps(chartDatasetsWithTwoWords)} />
+);
---
+++ front-end/src/routes/Auth/Signup/Signup.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Signup from './Signup';
+
+export default {
+  title: 'Routes/Auth',
+  component: Signup,
+};
+
+export const SignupForm = () => <Signup />;
