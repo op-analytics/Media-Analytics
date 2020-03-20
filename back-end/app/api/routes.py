f2f9d28c14d353cf74199e9dd24ea85000c8e9fb
Liam Christensen <39316713+Zimbardoex@users.noreply.github.com>
2020-03-20 17:37:51 +1300

    Remove old back end and containerise new back end (#236)
    * Delete back-end directory
    
    * Remove references to back-end from docker compose files
    
    * Remove references to back-end from nginx config
    
    * Add express-back-end dockerfiles
    
    * Add express to docker compose
    
    * Remove development nginx dockerfile
    
    * Remove old back-end from travis jobs
    
    Co-authored-by: Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>

--- back-end/app/api/routes.py
+++
@@ -1,9 +0,0 @@
-# Import flask dependencies
-from flask import Blueprint, jsonify
-
-routes = Blueprint("api", __name__)
-
-
-@routes.route("/", methods=["GET"])
-def index():
-    return jsonify({"code": 202, "message": "Hello from Media Analytics API"})
--- back-end/app/nlp/routes.py
+++
@@ -1,9 +0,0 @@
-# Import flask dependencies
-from flask import Blueprint, jsonify
-
-routes = Blueprint("nlp", __name__)
-
-
-@routes.route("/", methods=["GET"])
-def index():
-    return jsonify({"code": 202, "message": "Hello from nlp"})
