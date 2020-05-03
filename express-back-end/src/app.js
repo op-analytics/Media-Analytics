1f12eb50e8db7baa1ef62447a8c2d328f0f63b7c
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-03 14:42:40 +1200

    Connect to redis in backend

--- express-back-end/src/app.js
+++ express-back-end/src/app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require("cors");
+const cors = require('cors');
 
 const db = require('./db');
+const redis = require('./redis');
 const config = require('./config');
 
 const { checkTokenSetUser } = require('./api/auth/middlewares');
@@ -24,8 +25,12 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 
+// Connect to mongodb
 db.connect(config.mongooseURI);
 db.mongoose.set('debug', config.mongooseDebug);
+
+// Connect to redis
+redis.connect(config.redisURI);
 
 app.use(checkTokenSetUser);
 
