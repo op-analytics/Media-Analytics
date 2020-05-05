5b8cb36c742db84a64a6d30e92c6d22ee0e3e0b9
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-05 13:49:12 +1200

    Use request limiting on get frequency route

--- express-back-end/src/api/timeline/controllers/frequency.controller.js
+++ express-back-end/src/api/timeline/controllers/frequency.controller.js
@@ -1,5 +1,6 @@
 const Frequency = require('../models/frequency.model');
 const { FrequencySchema } = require('../schemas');
+const TokenHelper = require('../../auth/helpers/tokenHelper');
 
 module.exports = (function() {
   const sortOnKey = (objects, key) => {
@@ -9,6 +10,15 @@ module.exports = (function() {
   };
 
   const GetFrequency = async (req, res) => {
+    const userTokenHelper = new TokenHelper(req.user);
+
+    // Responed with an error if the user has no tokens available
+    if (!userTokenHelper.hasTokens()) {
+      res.status(429).json({
+        message: 'No tokens available to make this request',
+      });
+      return;
+    }
     const { error, value } = FrequencySchema.validate(req.body, {
       abortEarly: false,
       allowUnknown: true,
@@ -49,6 +59,8 @@ module.exports = (function() {
       // Sort each word's dataset by year
       sortOnKey(frequencyData, 'year');
       res.json({ data: frequencyData });
+      // Use one of the users tokens
+      userTokenHelper.useToken();
       return;
     }
     res.status(404).json({
