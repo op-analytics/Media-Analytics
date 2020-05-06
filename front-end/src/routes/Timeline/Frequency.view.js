5d731348558635ac78bab332949bf726b3494dd3
Zimbardoex <Zimbardoex@gmail.com>
2020-05-06 18:36:29 +1200

    Add card to frequency form and change button color

--- front-end/src/routes/Timeline/Frequency.view.js
+++ front-end/src/routes/Timeline/Frequency.view.js
@@ -3,6 +3,7 @@ import ChipInput from 'material-ui-chip-input';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -28,8 +29,9 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: 'white',
   },
   form: {
-    width: '45%',
+    width: '100%',
     height: '100%',
+    padding: '2rem',
   },
   input: {
     border: 'solid 1px black',
@@ -44,6 +46,10 @@ const useStyles = makeStyles(theme => ({
   },
   toggleButton: {
     maxWidth: '150px',
+  },
+  Card: {
+    width: '45vw',
+    minWidth: '400px',
   },
 }));
 
@@ -89,146 +95,148 @@ function Timeline() {
     <>
       <h3>Word Frequency Timeline</h3>
       <div className={classes.container}>
-        <form className={classes.form} onSubmit={onSubmitHandler}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <FormControl className={classes.formControl}>
-                <ChipInput
-                  label="Words:"
-                  name="words"
-                  newChipKeyCodes={[13, 32]} // Make new chip on enter and space key codes
-                  blurBehavior="add" // Fix android chrome bug
-                  onChange={newWords => setWords(newWords)}
-                  required={!words.length}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl className={classes.formControl}>
-                <Autocomplete
-                  multiple
-                  id="tags-standard"
-                  options={mediaOutlets}
-                  getOptionLabel={option => option.title}
-                  filterSelectedOptions
-                  required={!outlets.length}
-                  onChange={(_, value) =>
-                    setOutlets(value.map(({ value: code }) => code))
-                  }
-                  renderInput={params => (
-                    <TextField
-                      {...params}
-                      variant="standard"
-                      label="Media Outlets"
-                      placeholder="Add a media outlet"
-                    />
-                  )}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item md={3} sm={6} xs={12}>
-              <FormControl className={classes.formControl}>
-                <TextField
-                  className={classes.textField}
-                  type="number"
-                  label="Year from:"
-                  name="year_from"
-                  value={yearFrom}
-                  onChange={e => setYearFrom(Number(e.target.value))}
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid item md={3} sm={6} xs={12}>
-              <FormControl className={classes.formControl}>
-                <TextField
-                  className={classes.textField}
-                  type="number"
-                  label="Year to:"
-                  name="year_to"
-                  value={yearTo}
-                  onChange={e => setYearTo(Number(e.target.value))}
-                  required
-                />
-              </FormControl>
-            </Grid>
-            {
-              // Custom options start here
-            }
-            <Grid item md={3} sm={6} xs={12}>
-              <FormControl className={classes.formControl}>
-                <InputLabel
-                  className={classes.label}
-                  id="y-axis-key-select-label"
-                >
-                  Y-Axis Key
-                </InputLabel>
-                <Select
-                  labelId="y-axis-key-select-label"
-                  id="y-axis-key-select"
-                  value={yAxisKey}
-                  onChange={e => setYAxisKey(e.target.value)}
-                >
-                  <MenuItem value="freq">Frequency</MenuItem>
-                  <MenuItem value="count">Count</MenuItem>
-                  <MenuItem value="rank">Rank</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item md={3} sm={6} xs={12}>
-              <FormControl className={classes.formControl}>
-                <ToggleButton
-                  selected={absolute}
-                  value={absolute}
-                  onChange={() => {
-                    setAbsolute(!absolute);
-                  }}
-                  className={classes.ToggleButton}
-                >
-                  Display Absolute
-                </ToggleButton>
-              </FormControl>
-            </Grid>
-            <Grid item md xs={12}>
-              <FormControl className={classes.formControl}>
-                <InputLabel
-                  className={classes.label}
-                  id="display-option-select-label"
-                >
-                  Display Options
-                </InputLabel>
-                <Select
-                  labelId="display-option-select-label"
-                  id="display-option-select"
-                  value={displayOption}
-                  onChange={e => setDisplayOption(e.target.value)}
-                >
-                  {displayOptions.map(option => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            {
-              // Submit button
-            }
-            <Grid container item sm={12} justify="center">
-              <Grid item xs={3} align="center">
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  type="submit"
-                  className={classes.submitButton}
-                  margin="0 auto"
-                >
-                  Submit
-                </Button>
+        <Card className={classes.Card}>
+          <form className={classes.form} onSubmit={onSubmitHandler}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControl className={classes.formControl}>
+                  <ChipInput
+                    label="Words:"
+                    name="words"
+                    newChipKeyCodes={[13, 32]} // Make new chip on enter and space key codes
+                    blurBehavior="add" // Fix android chrome bug
+                    onChange={newWords => setWords(newWords)}
+                    required={!words.length}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl className={classes.formControl}>
+                  <Autocomplete
+                    multiple
+                    id="tags-standard"
+                    options={mediaOutlets}
+                    getOptionLabel={option => option.title}
+                    filterSelectedOptions
+                    required={!outlets.length}
+                    onChange={(_, value) =>
+                      setOutlets(value.map(({ value: code }) => code))
+                    }
+                    renderInput={params => (
+                      <TextField
+                        {...params}
+                        variant="standard"
+                        label="Media Outlets"
+                        placeholder="Add a media outlet"
+                      />
+                    )}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item md={3} sm={6} xs={12}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    className={classes.textField}
+                    type="number"
+                    label="Year from:"
+                    name="year_from"
+                    value={yearFrom}
+                    onChange={e => setYearFrom(Number(e.target.value))}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item md={3} sm={6} xs={12}>
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    className={classes.textField}
+                    type="number"
+                    label="Year to:"
+                    name="year_to"
+                    value={yearTo}
+                    onChange={e => setYearTo(Number(e.target.value))}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              {
+                // Custom options start here
+              }
+              <Grid item md={3} sm={6} xs={12}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel
+                    className={classes.label}
+                    id="y-axis-key-select-label"
+                  >
+                    Y-Axis Key
+                  </InputLabel>
+                  <Select
+                    labelId="y-axis-key-select-label"
+                    id="y-axis-key-select"
+                    value={yAxisKey}
+                    onChange={e => setYAxisKey(e.target.value)}
+                  >
+                    <MenuItem value="freq">Frequency</MenuItem>
+                    <MenuItem value="count">Count</MenuItem>
+                    <MenuItem value="rank">Rank</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item md={3} sm={6} xs={12}>
+                <FormControl className={classes.formControl}>
+                  <ToggleButton
+                    selected={absolute}
+                    value={absolute}
+                    onChange={() => {
+                      setAbsolute(!absolute);
+                    }}
+                    className={classes.ToggleButton}
+                  >
+                    Display Absolute
+                  </ToggleButton>
+                </FormControl>
+              </Grid>
+              <Grid item md xs={12}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel
+                    className={classes.label}
+                    id="display-option-select-label"
+                  >
+                    Display Options
+                  </InputLabel>
+                  <Select
+                    labelId="display-option-select-label"
+                    id="display-option-select"
+                    value={displayOption}
+                    onChange={e => setDisplayOption(e.target.value)}
+                  >
+                    {displayOptions.map(option => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+              {
+                // Submit button
+              }
+              <Grid container item sm={12} justify="center">
+                <Grid item xs={3} align="center">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    className={classes.submitButton}
+                    margin="0 auto"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </form>
+          </form>
+        </Card>
 
         {loading ? (
           <CircularProgress />
