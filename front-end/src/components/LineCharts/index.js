2cd3d13a51ea7c3e983579326247d32cbd146e33
horrjd1 <jamie489horrell@gmail.com>
2020-05-10 17:04:30 +1200

    Add download as csv option per chart

--- front-end/src/components/LineCharts/index.js
+++ front-end/src/components/LineCharts/index.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
@@ -11,14 +12,15 @@ import {
   YAxis,
 } from 'recharts';
 import { createTooltip } from './utils';
+import { CSVLink } from 'react-csv';
 
 const useStyles = makeStyles(() => ({
   chartContainer: {
     width: '80vw',
-    height: '50vh',
+    height: '60vh',
     flex: '0 1 auto',
     maxWidth: '1000px',
-    paddingBottom: '6vh',
+    paddingBottom: '12vh',
   },
   chartTitle: {
     textAlign: 'center',
@@ -58,7 +60,6 @@ function LineCharts({
   displayAbsolute,
 }) {
   const classes = useStyles();
-
   return (
     <>
       {datasets.map(data => (
@@ -93,6 +94,11 @@ function LineCharts({
               />
             </LineChart>
           </ResponsiveContainer>
+          <Box display="flex" justifyContent="flex-end" p={3}>
+            <CSVLink data={data.data} filename={data.title + '-word-freq.csv'}>
+              Download as CSV
+            </CSVLink>
+          </Box>
         </div>
       ))}
     </>
