326214f3416e25227cb4f68160327d6de5087748
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-11 15:47:10 +1200

    Clean up latentAssociation and frequency controllers

--- express-back-end/src/api/timeline/controllers/frequency.controller.js
+++ express-back-end/src/api/timeline/controllers/frequency.controller.js
@@ -1,63 +1,45 @@
 const Frequency = require('../models/frequency.model');
 const { FrequencySchema } = require('../schemas');
 
-module.exports = (function() {
-  const sortOnKey = (objects, key) => {
-    for (object of objects) {
-      object.data.sort((x, y) => x[key] - y[key]);
-    }
-  };
+function sortOnKey(objects, key) {
+  for (object of objects) {
+    object.data.sort((x, y) => x[key] - y[key]);
+  }
+}
 
-  const GetFrequency = async (req, res) => {
-    const { error, value } = FrequencySchema.validate(req.body, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-    if (error) {
-      res.status(422).json({
-        errors: error.details.map(error => ({
-          value: error.path,
-          message: error.message,
-        })),
-      });
-      return;
+function shapeData(dbData) {
+  let frequencyData = [];
+  for (frequencyDataObj of dbData) {
+    let word = frequencyDataObj.word;
+    let dataIndex = frequencyData.findIndex(fd => fd.word == word);
+    let wordData = {
+      year: frequencyDataObj.year,
+      rank: frequencyDataObj.rank,
+      count: frequencyDataObj.count,
+      freq: frequencyDataObj.freq,
+    };
+    if (dataIndex == -1) {
+      frequencyData.push({ word: word, data: [wordData] });
+    } else {
+      // If the word already exists in frequency data,
+      // just append the current year's data.
+      frequencyData[dataIndex].data.push(wordData);
     }
-    const cleanedWords = value.words.map((word)=>word.trim().toLowerCase());
-    const frequencyDataDB = await Frequency.find({
-      word: cleanedWords,
-      year: { $gte: value.year_from, $lte: value.year_to },
-    });
+  }
+  return frequencyData;
+}
 
-    if (frequencyDataDB.length > 0) {
-      let frequencyData = [];
-      for (frequencyDataObj of frequencyDataDB) {
-        let word = frequencyDataObj.word;
-        let dataIndex = frequencyData.findIndex(fd => fd.word == word);
-        let wordData = {
-          year: frequencyDataObj.year,
-          rank: frequencyDataObj.rank,
-          count: frequencyDataObj.count,
-          freq: frequencyDataObj.freq,
-        };
-        if (dataIndex == -1) {
-          frequencyData.push({ word: word, data: [wordData] });
-        } else {
-          // If the word already exists in frequency data,
-          // just append the current year's data.
-          frequencyData[dataIndex].data.push(wordData);
-        }
-      }
-      // Sort each word's dataset by year
-      sortOnKey(frequencyData, 'year');
-      res.json({ data: frequencyData });
-      return;
-    }
-    res.status(404).json({
-      error: 'No frequency data was found for given parameters',
-    });
-  };
+async function GetFrequency(words, yearFrom, yearTo) {
+  const frequencyDataDB = await Frequency.find({
+    word: words,
+    year: { $gte: yearFrom, $lte: yearTo },
+  });
+  const frequencyData = shapeData(frequencyDataDB);
 
-  return {
-    GetFrequency,
-  };
-})();
+  sortOnKey(frequencyData, 'year');
+  return frequencyData;
+}
+
+module.exports = {
+  GetFrequency,
+};
--- express-back-end/src/api/timeline/timeline.router.js
+++ express-back-end/src/api/timeline/timeline.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const LatentAssociationController = require('./controllers/latentAssociation.controller');
 const FrequencyController = require('./controllers/frequency.controller');
 const { ensureLoggedIn } = require('../auth/middlewares');
+const { validateBody } = require('../middlewares/validation');
+const { FrequencySchema, LatentAssociationSchema } = require('../schemas');
 
 router.get('/', (_, res) => {
   res.json({ message: 'You reached timeline' });
@@ -10,10 +12,48 @@ router.get('/', (_, res) => {
 
 router.post(
   '/latent-association',
-  ensureLoggedIn,
-  LatentAssociationController.GetLatentAssociation,
+  [ensureLoggedIn, validateBody(LatentAssociationSchema)],
+  async (req, res) => {
+    const {
+      concept_1: concept1,
+      concept_2: concept2,
+      year_from: yearFrom,
+      year_to: yearTo,
+    } = req;
+    concept1 = LatentAssociationController.CleanConcept(concept1);
+    concept2 = LatentAssociationController.CleanConcept(concept2);
+    const latentAssociationData = await LatentAssociationController.GetLatentAssociation(
+      concept1,
+      concept2,
+      yearFrom,
+      yearTo,
+    );
+    if (latentAssociationData.length > 0)
+      return res.json({ data: latentAssociationData });
+
+    res.status(404).json({
+      error: 'No latent association data was found for given parameters',
+    });
+  },
 );
 
-router.post('/frequency', ensureLoggedIn, FrequencyController.GetFrequency);
+router.post(
+  '/frequency',
+  [ensureLoggedIn, validateBody(FrequencySchema)],
+  async (req, res) => {
+    const { words, year_from, year_to } = req;
+    words = req.words.map(word => word.trim().toLowerCase());
+
+    const frequencyData = await FrequencyController.GetFrequency(
+      words,
+      yearFrom,
+      yearTo,
+    );
+    if (frequencyData.length > 0) return res.json({ data: frequencyData });
+    res.status(404).json({
+      error: 'No frequency data was found for given parameters',
+    });
+  },
+);
 
 module.exports = router;
