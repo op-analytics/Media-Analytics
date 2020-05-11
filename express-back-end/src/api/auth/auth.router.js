ba02895b4fc5f6ad9cae7a72b91b5195f20138bd
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-11 15:08:49 +1200

    Clean up user controller signup and get user

--- express-back-end/src/api/auth/auth.router.js
+++ express-back-end/src/api/auth/auth.router.js
@@ -1,14 +1,50 @@
+const { SignupSchema, LoginSchema } = require('./schemas');
+const User = require('../models/user.model');
 const express = require('express');
 const router = express.Router();
 const UserController = require('./controllers/users.controller');
 const { ensureLoggedIn } = require('./middlewares');
+const { validateBody } = require('../middlewares/validation');
+
+const createValidationError = (message, type = ['general']) => ({
+  type,
+  message,
+});
 
 router.get('/', (_, res) => {
   res.json({ message: 'You reached auth' });
 });
 
-router.post('/signup', UserController.Signup);
+router.post('/signup', validateBody(SignupSchema), async (req, res) => {
+  const { email, name, password } = req;
+  email = email.toLowerCase();
+
+  // Check email is not already taken
+  if (await UserController.EmailTaken(req.email)) {
+    return res.status(422).json({
+      errors: [createValidationError('Email already taken', ['email'])],
+    });
+  }
+
+  // Try signup user but responed with an error if it fails
+  try {
+    const token = await UserController.Signup(name, email, password);
+    res.json({
+      token,
+    });
+  } catch (e) {
+    res
+      .status(500)
+      .json(createValidationError('There was an error creating your user'));
+  }
+});
+
 router.post('/login', UserController.Login);
-router.get('/user', ensureLoggedIn, UserController.GetUserData);
+
+router.get('/user', ensureLoggedIn, (req, res) => {
+  res.json({
+    data: { ...req.user },
+  });
+});
 
 module.exports = router;
--- express-back-end/src/api/auth/controllers/users.controller.js
+++ express-back-end/src/api/auth/controllers/users.controller.js
@@ -4,129 +4,94 @@ const config = require('../../../config');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-module.exports = (function() {
-  const createValidationError = (message, type = ['general']) => ({
-    type,
-    message,
+const createValidationError = (message, type = ['general']) => ({
+  type,
+  message,
+});
+
+function tokenizeUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    config.secret,
+    {
+      expiresIn: '4h',
+    },
+  );
+}
+
+async function EmailTaken(email) {
+  return await User.findOne({ email });
+}
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(UserData.password, salt);
+}
+
+async function Signup(name, email, password) {
+  const hashedPassword = await hashedPassword();
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
   });
 
-  const tokenizeUser = user =>
-    jwt.sign(
-      {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-      },
-      config.secret,
-      {
-        expiresIn: '4h',
-      },
-    );
+  const result = await user.save();
 
-  const GetUserData = (req, res) => {
-    res.json({
-      data: { ...req.user },
+  // Tokenize data
+  return tokenizeUser(result);
+}
+
+async function Login(req, res) {
+  // Validate against schema
+  const { error, value: UserData } = LoginSchema.validate(req.body, {
+    abortEarly: false,
+    allowUnknown: true,
+  });
+  if (error) {
+    res.status(422).json({
+      errors: error.details.map(error =>
+        createValidationError(error.message, error.path),
+      ),
     });
-  };
+    return;
+  }
+  UserData.email = UserData.email.toLowerCase();
 
-  const Signup = async (req, res) => {
-    // Validate against schema
-    const { error, value: UserData } = SignupSchema.validate(req.body, {
-      abortEarly: false,
-      allowUnknown: true,
+  // Try get user with email
+  const user = await User.findOne({ email: UserData.email });
+  if (!user) {
+    res.status(400).json({
+      errors: [createValidationError('Incorrect information')],
     });
-    if (error) {
-      res.status(422).json({
-        errors: error.details.map(error =>
-          createValidationError(error.message, error.path),
-        ),
-      });
-      return;
-    }
+    return;
+  }
 
-    UserData.email = UserData.email.toLowerCase();
-
-    // Check email is not already taken
-    const emailAlreadyTaken = await User.findOne({ email: UserData.email });
-    if (emailAlreadyTaken) {
-      res.status(422).json({
-        errors: [createValidationError('Email already taken', ['email'])],
-      });
-      return;
-    }
-
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(UserData.password, salt);
-
-    // Try save and tokenize the user return error if it doesn't work
-    const user = new User({
-      ...UserData,
-      password: hashedPassword,
+  // Check passwords match
+  const validPass = await bcrypt.compare(UserData.password, user.password);
+  if (!validPass) {
+    res.status(400).json({
+      errors: [createValidationError('Incorrect information')],
     });
+    return;
+  }
 
-    try {
-      const result = await user.save();
+  //tokenise users data and return it
+  const token = tokenizeUser(user);
 
-      // Tokenize data
-      const token = tokenizeUser(result);
+  res.json({
+    token,
+  });
+}
 
-      // return the tokenized user
-      res.json({
-        token,
-      });
-    } catch (err) {
-      res
-        .status(500)
-        .json(createValidationError('There was an error creating your user'));
-      return;
-    }
-  };
-
-  const Login = async (req, res) => {
-    // Validate against schema
-    const { error, value: UserData } = LoginSchema.validate(req.body, {
-      abortEarly: false,
-      allowUnknown: true,
-    });
-    if (error) {
-      res.status(422).json({
-        errors: error.details.map(error =>
-          createValidationError(error.message, error.path),
-        ),
-      });
-      return;
-    }
-    UserData.email = UserData.email.toLowerCase();
-
-    // Try get user with email
-    const user = await User.findOne({ email: UserData.email });
-    if (!user) {
-      res.status(400).json({
-        errors: [createValidationError('Incorrect information')],
-      });
-      return;
-    }
-
-    // Check passwords match
-    const validPass = await bcrypt.compare(UserData.password, user.password);
-    if (!validPass) {
-      res.status(400).json({
-        errors: [createValidationError('Incorrect information')],
-      });
-      return;
-    }
-
-    //tokenise users data and return it
-    const token = tokenizeUser(user);
-
-    res.json({
-      token,
-    });
-  };
-  return {
-    Signup,
-    Login,
-    GetUserData,
-  };
-})();
+module.exports = {
+  Signup,
+  Login,
+  GetUserData,
+  CheckEmailTaken: EmailTaken,
+};
