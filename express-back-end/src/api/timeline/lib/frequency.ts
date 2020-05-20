63bac732b3c35e55b6eae53307f5bb120a7422f3
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-20 21:38:44 +1200

    Fix bug and minor issues in frequency portion of the app
    - Fix bug causing app to crash {not using $in to query from array}
    - Remove unnecessary any types in Frequency lib file

--- express-back-end/src/api/timeline/lib/frequency.ts
+++ express-back-end/src/api/timeline/lib/frequency.ts
@@ -1,6 +1,15 @@
-import Frequency from '../models/frequency.model';
+import Frequency, { FrequencyDocument } from '../models/frequency.model';
 
-// TODO: Fix the types in this file
+interface FrequencyData {
+  word: string;
+  data: {
+    year: number;
+    rank: number;
+    count: number;
+    freq: number;
+  }[];
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function sortOnKey(objects: { data: Record<string, any> }[], key: string): void {
   objects.forEach(object => {
@@ -11,50 +20,25 @@ function sortOnKey(objects: { data: Record<string, any> }[], key: string): void 
   });
 }
 
-function shapeData(
-  dbData: {
-    year: number;
-    rank: number;
-    count: number;
-    freq: number;
-    word: string;
-  }[],
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): any {
-  const frequencyData: {
-    word: string;
-    data: {
-      year: number;
-      rank: number;
-      count: number;
-      freq: number;
-    }[];
-  }[] = [];
-  dbData.forEach(
-    (frequencyDataObj: {
-      year: number;
-      rank: number;
-      count: number;
-      freq: number;
-      word: string;
-    }) => {
-      const { word } = frequencyDataObj;
-      const dataIndex = frequencyData.findIndex(fd => fd.word === word);
-      const wordData = {
-        year: frequencyDataObj.year,
-        rank: frequencyDataObj.rank,
-        count: frequencyDataObj.count,
-        freq: frequencyDataObj.freq,
-      };
-      if (dataIndex === -1) {
-        frequencyData.push({ word, data: [wordData] });
-      } else {
-        // If the word already exists in frequency data,
-        // just append the current year's data.
-        frequencyData[dataIndex].data.push(wordData);
-      }
-    },
-  );
+function shapeData(dbData: FrequencyDocument[]): FrequencyData[] {
+  const frequencyData: FrequencyData[] = [];
+  dbData.forEach((frequencyDataObj: FrequencyDocument) => {
+    const { word } = frequencyDataObj;
+    const dataIndex = frequencyData.findIndex(fd => fd.word === word);
+    const wordData = {
+      year: frequencyDataObj.year,
+      rank: frequencyDataObj.rank,
+      count: frequencyDataObj.count,
+      freq: frequencyDataObj.freq,
+    };
+    if (dataIndex === -1) {
+      frequencyData.push({ word, data: [wordData] });
+    } else {
+      // If the word already exists in frequency data,
+      // just append the current year's data.
+      frequencyData[dataIndex].data.push(wordData);
+    }
+  });
   return frequencyData;
 }
 
@@ -62,19 +46,12 @@ async function GetFrequency(
   words: string[],
   yearFrom: number,
   yearTo: number,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Promise<any> {
-  // TODO: Check if there is a better way to do this
-  const frequencyDataDB = ((await Frequency.find({
-    word: words,
+): Promise<FrequencyData[]> {
+  const frequencyDataDB = await Frequency.find({
+    word: { $in: words },
     year: { $gte: yearFrom, $lte: yearTo },
-  }).exec()) as unknown) as {
-    year: number;
-    rank: number;
-    count: number;
-    freq: number;
-    word: string;
-  }[];
+  }).exec();
+
   const frequencyData = shapeData(frequencyDataDB);
 
   sortOnKey(frequencyData, 'year');
