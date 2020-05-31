bb3a755b803a21dc2d66eab76dfb6756a62c6384
NICHTJ3 <nicholsontrent@gmail.com>
2020-05-31 20:52:29 +1200

    Add ability to use @/* to import from root of backend to the tsconfig and eslint setup

--- express-back-end/.eslintrc.js
+++ express-back-end/.eslintrc.js
@@ -1,31 +1,23 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.ts'],
-      },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  settings:{
+   "import/resolver": {
+      typescript: {} // this loads <rootdir>/tsconfig.json to eslint
     },
   },
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     'prettier/@typescript-eslint', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'airbnb-base',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
   rules: {
     'no-underscore-dangle': 0,
     'arrow-parens': 0,
