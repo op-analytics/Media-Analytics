32886ac76e8453ff9ab59a2c655eb627f5ccb391
horrjd1 <jamie489horrell@gmail.com>
2020-05-31 18:11:51 +1200

    Change csv download class to function

--- front-end/src/components/CsvDownloadButton.js
+++ front-end/src/components/CsvDownloadButton.js
@@ -1,13 +1,12 @@
 import Fab from '@material-ui/core/Fab';
 import GetAppIcon from '@material-ui/icons/GetApp';
 import TooltipUI from '@material-ui/core/Tooltip';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { CSVLink } from 'react-csv';
 
-
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
   fab: {
     position: 'fixed',
     bottom: 20,
@@ -17,31 +16,25 @@ const useStyles = theme => ({
     display: 'block',
     margin: 'auto',
   },
-});
+}));
 
-class CsvDownloadButton extends React.Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <TooltipUI title="Download as csv" placement="right" arrow>
-        <Fab color="secondary" aria-label="download" className={classes.fab}>
-          <CSVLink
-            data={this.props.data}
-            headers={this.props.headers}
-            filename={this.props.filename}
-          >
-            <GetAppIcon fontSize="large" className={classes.icon} />
-          </CSVLink>
-        </Fab>
-      </TooltipUI>
-    );
-  }
+function CsvDownloadButton({ data, headers, filename }) {
+  const classes = useStyles();
+  return (
+    <TooltipUI title="Download as csv" placement="right" arrow>
+      <Fab color="secondary" aria-label="download" className={classes.fab}>
+        <CSVLink data={data} headers={headers} filename={filename}>
+          <GetAppIcon fontSize="large" className={classes.icon} />
+        </CSVLink>
+      </Fab>
+    </TooltipUI>
+  );
 }
 
 CsvDownloadButton.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
   headers: PropTypes.array,
-  filename: PropTypes.string
-}
+  filename: PropTypes.string,
+};
 
-export default withStyles(useStyles)(CsvDownloadButton);
+export default CsvDownloadButton;
