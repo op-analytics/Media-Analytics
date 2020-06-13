065cb7a6d92c900dd8e80bf7696337ad30bbdb49
Zimbardoex <Zimbardoex@gmail.com>
2020-06-14 10:40:20 +1200

    Remove sentiment and latent association from user guide

--- front-end/src/routes/UserGuide/index.js
+++ front-end/src/routes/UserGuide/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import { useSectionStyles } from './hooks/useSectionStyles';
 import FrequencyCounts from './sections/FrequencyCounts';
-import LatentAssociations from './sections/LatentAssociations';
-import SentimentAnalysis from './sections/SentimentAnalysis';
 
 const About = () => {
   const classes = useSectionStyles();
@@ -23,8 +21,6 @@ const About = () => {
           </p>
         </div>
         <FrequencyCounts />
-        <LatentAssociations />
-        <SentimentAnalysis />
       </Card>
     </Container>
   );
