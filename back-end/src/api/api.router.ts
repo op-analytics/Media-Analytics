5203346eac41ff6f890ca493ddb2f26531d88306
Trent Nicholson <39316919+NICHTJ3@users.noreply.github.com>
2020-06-22 19:17:03 +1200

    Add documentation using swagger for the api (#396)
    Co-authored-by: Jamie Horrell <jamie489horrell@gmail.com>
    Co-authored-by: Trent Nicholson <nichtj3@student.op.ac.nz>
    Co-authored-by: Liam Christensen <39316713+Zimbardoex@users.noreply.github.com>

--- back-end/src/api/api.router.ts
+++ back-end/src/api/api.router.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 
 import auth from './auth/auth.router';
+import swaggerDocument from '../swagger.json';
+import swaggerUi from 'swagger-ui-express';
 import timeline from './timeline/timeline.router';
 
 const router = express.Router();
 
-router.get('/', (_, res) => {
-  res.json({ message: 'Welcome to the NYT media analytics api' });
-});
+// Serve the swagger.json file as the root route
+router.use('/', swaggerUi.serve)
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 router.use('/timeline', timeline);
 router.use('/auth', auth);
