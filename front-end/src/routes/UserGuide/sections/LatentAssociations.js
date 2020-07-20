f9c7430f063cfb52c33ac2f22fb5b223c765b286
NICHTJ3 <nicholsontrent@gmail.com>
2020-07-20 12:33:46 +1200

    Add section for interpreting the la data

--- front-end/src/routes/UserGuide/sections/LatentAssociations.js
+++ front-end/src/routes/UserGuide/sections/LatentAssociations.js
@@ -22,21 +22,8 @@ export default function LatentAssociations() {
       >
         Concepts are groupings of words that represent an idea, words can be
         added to a concept via a chip input in the same way as words on the
-        frequency page.
-      </Typography>
-      {/* TODO: Change this image */}
-      <img
-        src="/assets/userguide/frequency-word-remove.png"
-        className={classes.image}
-        alt="Input pane"
-      />
-      <Typography
-        variant="body1"
-        component="p"
-        color="textPrimary"
-        className={classes.aboutText}
-      >
-        You may add up to four words per concept in a single request.
+        frequency page. You may add up to four words per concept in a single
+        request.
       </Typography>
 
       <h3 className={classes.subSectionHeadings}>Choosing an outlet</h3>
@@ -46,12 +33,23 @@ export default function LatentAssociations() {
         color="textPrimary"
         className={classes.aboutText}
       >
-        Choosing an outlet is much the same as choosing an outlet on the
+        Choosing an outlet is the same as choosing an outlet on the
         FrequencyCounts page, although you may only choose one of the available
         outlets in a single request.
       </Typography>
 
       <h3 className={classes.subSectionHeadings}>Interpreting the data</h3>
+      <Typography
+        variant="body1"
+        component="p"
+        color="textPrimary"
+        className={classes.aboutText}
+      >
+        The latent association metric between a pair of words is derived from the
+        cosine similarity of the word2vec embedding vector representations of
+        each word. This metric can be interpreted as measuring the likelihood
+        with which a pair of words co-occur together or in similar contexts.
+      </Typography>
     </div>
   );
 }
